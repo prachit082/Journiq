03c5c09431271dd5703d172512ad1a06ed8675fd
Prachit Pandit <20scs047.prachit.16@gmail.com>
2025-07-09 10:46:13 +0530

    Implemented Admin Dashboard UI

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,3 +1,5 @@
+import { formatDate } from "~/lib/utils";
+
 export const sidebarItems = [
   {
     id: 1,
@@ -18,3 +20,83 @@ export const sidebarItems = [
     href: "/trips",
   },
 ];
+
+export const user = {
+    name: "Prachit",
+  };
+
+export const users = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+      imageUrl: "/assets/images/david.webp",
+      dateJoined: formatDate("2025-01-01"),
+      itineraryCreated: 10,
+      status: "user",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+      imageUrl: "/assets/images/david.webp",
+      dateJoined: formatDate("2025-01-02"),
+      itineraryCreated: 4,
+      status: "user",
+    },
+    {
+      id: 3,
+      name: "John Smith",
+      email: "john.smith@example.com",
+      imageUrl: "/assets/images/david.webp",
+      dateJoined: formatDate("2025-01-03"),
+      itineraryCreated: 8,
+      status: "admin",
+    },
+  ];
+
+export const dashboardStats = {
+    totalUsers: 1237,
+    usersJoined: { currentMonth: 218, lastMonth: 176 },
+    totalTrips: 118,
+    tripsCreated: { currentMonth: 6, lastMonth: 2 },
+    userRole: { total: 62, currentMonth: 63, lastMonth: 78 },
+  };
+
+export const allTrips = [{
+      id: 1,
+      name: "Tropical Rewind",
+      imageUrls: ["/assets/images/sample1.jpg"],
+      itinerary: [{ location: "Thailand" }],
+      tags: ["Adventure", "Culture"],
+      travelStyle: "Solo",
+      estimatedPrice: "$1,000",
+    },
+    {
+      id: 2,
+      name: "French Reverie",
+      imageUrls: ["/assets/images/sample2.jpg"],
+      itinerary: [{ location: "Paris" }],
+      tags: ["Relaxation", "Culinary"],
+      travelStyle: "Family",
+      estimatedPrice: "$2,000",
+    },
+    {
+      id: 3,
+      name: "Zen Break",
+      imageUrls: ["/assets/images/sample3.jpg"],
+      itinerary: [{ location: "Japan" }],
+      tags: ["Shopping", "Luxury"],
+      travelStyle: "Couple",
+      estimatedPrice: "$3,000",
+    },
+    {
+      id: 4,
+      name: "Adventure in Westeros",
+      imageUrls: ["/assets/images/sample4.jpg"],
+      itinerary: [{ location: "Croatia" }],
+      tags: ["Historical", "Culture"],
+      travelStyle: "Friends",
+      estimatedPrice: "$4,000",
+    },
+    ];
